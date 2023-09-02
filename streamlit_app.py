1d65bdd71848d8c5a9617fb0177ca8027c98d7b7
GMendozaSF <143445186+GMendozaSF@users.noreply.github.com>
2023-09-02 01:34:43 -0500

    Update streamlit_app.py
    Added ability for user to input their own fruits and data is then pulled from Fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,10 @@ streamlit.dataframe(fruits_to_show)
 #New section to display the fruityvice api response and a new header
 streamlit.header('Fruityvice Fruit Advice!')
 
+#Adding a text entry box and sending the input to the Fruityvice API
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #pulling the data from the API
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
